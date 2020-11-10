ccc33309e2dee164a49c4b9cc5a52bfd01728057
Felix Auringer <48409110+felixauringer@users.noreply.github.com>
2020-11-10 14:17:42 +0100

    Add failing feature specs for new author input fields

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -5,4 +5,20 @@ describe "New author page", type: :feature do
   it "should render withour error" do
     visit new_author_path
   end
+
+  describe 'input fields' do
+    before { visit new_author_path }
+
+    it 'has a first_name input field' do
+      expect(page).to have_field('author[first_name]')
+    end
+
+    it 'has a last_name input field' do
+      expect(page).to have_field('author[last_name]')
+    end
+
+    it 'has a homepage input field' do
+      expect(page).to have_field('author[homepage]')
+    end
+  end
 end
