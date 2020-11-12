64d356550d9719ce4f61dc7050f1bad4d5fd4669
Felix Auringer <48409110+felixauringer@users.noreply.github.com>
2020-11-12 21:29:41 +0100

    Add failing request spec for edit author with invalid id

--- spec/requests/authors/edit_spec.rb
+++ spec/requests/authors/edit_spec.rb
@@ -1,11 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Edit author request', type: :request do
-  let(:author) { FactoryBot.create :author }
+  describe 'with valid id' do
+    let(:author) { FactoryBot.create :author }
 
-  it 'renders the edit author template' do
-    get edit_author_path(author)
-    expect(response).to have_http_status :ok
-    expect(response).to render_template :edit
+    it 'renders the edit author template' do
+      get edit_author_path(author)
+      expect(response).to have_http_status :ok
+      expect(response).to render_template :edit
+    end
+  end
+
+  describe 'with invalid id' do
+    let(:invalid_author_id) { 42 }
+
+    it 'renders the not found page' do
+      expect { get edit_author_path(invalid_author_id) }.to raise_error(ActionController::RoutingError)
+    end
   end
 end
