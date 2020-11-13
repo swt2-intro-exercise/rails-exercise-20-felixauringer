1deac8ce917b27f2bc16c5fceb698520208402dc
Felix Auringer <48409110+felixauringer@users.noreply.github.com>
2020-11-13 12:00:43 +0100

    Add failing view test for author list on paper page

--- spec/views/papers/show.html.erb_spec.rb
+++ spec/views/papers/show.html.erb_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'papers/show', type: :view do
+  let(:authors) { FactoryBot.create_list(:author, 5) }
   before(:each) do
-    @paper = assign(:paper, Paper.create!(
+    @paper = assign(:paper, Paper.create(
                               title: 'Title',
                               venue: 'Venue',
-                              year: 2
+                              year: 2,
+                              authors: authors
                             ))
   end
 
@@ -14,5 +16,7 @@ RSpec.describe 'papers/show', type: :view do
     expect(rendered).to match(/Title/)
     expect(rendered).to match(/Venue/)
     expect(rendered).to match(/2/)
+    expect(rendered).to have_text(authors[0].name)
+    expect(rendered).to have_text(authors[-1].name)
   end
 end
